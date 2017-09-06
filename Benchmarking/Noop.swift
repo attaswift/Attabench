abf18eb6883a7e8c1383fee0708047a98d00dbe4
Károly Lőrentey <karoly@lorentey.hu>
2017-09-06 14:41:46 +0200

    Move Benchmarking & BenchmarkingIPC to separate package.

--- Benchmarking/Noop.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © 2017 Károly Lőrentey.
-// This file is part of Attabench: https://github.com/lorentey/Attabench
-// For licensing information, see the file LICENSE.md in the Git repository above.
-
-@_semantics("optimize.sil.never")
-@inline(never)
-public func noop<T>(_ value: T) {
-    _fixLifetime(value)
-}
--- Package.swift
+++ Package.swift
@@ -4,22 +4,19 @@ import PackageDescription
 let package = Package(
     name: "Attabench",
     products: [
-        .library(name: "Benchmarking", targets: ["Benchmarking", "BenchmarkIPC"]),
         .library(name: "BenchmarkModel", targets: ["BenchmarkModel"]),
-        .library(name: "BenchmarkRunner", targets: ["BenchmarkRunner", "BenchmarkIPC"]),
+        .library(name: "BenchmarkRunner", targets: ["BenchmarkRunner"]),
         .library(name: "BenchmarkCharts", targets: ["BenchmarkCharts"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/attaswift/OptionParser", .branch("master")),
+        .package(url: "https://github.com/attaswift/Benchmarking", .branch("master")),
         .package(url: "https://github.com/attaswift/BigInt", .branch("swift4")),
         .package(url: "https://github.com/attaswift/SipHash", .branch("swift4")),
         .package(url: "https://github.com/attaswift/GlueKit", .branch("master")),
     ],
     targets: [
-        .target(name: "BenchmarkIPC", path: "BenchmarkIPC"),
-        .target(name: "Benchmarking", dependencies: ["OptionParser", "BenchmarkIPC"], path: "Benchmarking"),
         .target(name: "BenchmarkModel", dependencies: ["BigInt", "GlueKit"], path: "BenchmarkModel"),
-        .target(name: "BenchmarkRunner", dependencies: ["BenchmarkIPC", "BenchmarkModel"], path: "BenchmarkRunner"),
+        .target(name: "BenchmarkRunner", dependencies: ["Benchmarking", "BenchmarkModel"], path: "BenchmarkRunner"),
         .target(name: "BenchmarkCharts", dependencies: ["BenchmarkModel"], path: "BenchmarkCharts"),
     ],
     swiftLanguageVersions: [4]
