cdd3e0446cade484b618a27cc19301c265a88da5
X140Yu <zhaoxinyu1994@gmail.com>
2018-12-14 14:40:03 +0800

    Fix BenchmarkExamples by bumping Benchmarking package version

--- SampleBenchmark.attabench/Package.swift
+++ SampleBenchmark.attabench/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .executable(name: "Benchmark", targets: ["Benchmark"])
     ],
     dependencies: [
-    .package(url: "https://github.com/attaswift/Benchmarking", from: "1.0.0")
+    .package(url: "https://github.com/attaswift/Benchmarking", .branch("master"))
     ],
     targets: [
         .target(name: "Benchmark", dependencies: ["Benchmarking"], path: "Sources"),
